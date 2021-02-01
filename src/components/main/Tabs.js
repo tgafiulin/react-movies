d8a8fcb812dc4da4761ee4e514457c02487b0a02
tgafiulin <t.gafiulin@ya.ru>
2021-02-01 14:47:45 +0300

    change tab component output

--- src/components/main/Tabs.js
+++ src/components/main/Tabs.js
@@ -5,20 +5,11 @@ import TabContent from './TabContent'
 function Tabs () {
     const [showMovies, toggleShowMovies] = useState(true);
 
-    let classNameMovies = "tab tab-movies";
-    let classNameTv = "tab tab-tv";
-
-    if (showMovies) {
-        classNameMovies += ' tab--active';
-    } else {
-        classNameTv += ' tab--active';
-    }
-
     return (
         <Fragment>
             <div className="tabs">
-                <div className={classNameMovies} onClick={() => toggleShowMovies(true)}>Фильмы</div>
-                <div className={classNameTv} onClick={() =>toggleShowMovies(false)}>Телеканалы</div>
+                <div className={showMovies ? 'tab tab--active' : 'tab'} onClick={() => toggleShowMovies(true)}>Фильмы</div>
+                <div className={!showMovies ? 'tab tab--active' : 'tab'} onClick={() =>toggleShowMovies(false)}>Телеканалы</div>
             </div>
             <TabContent showMovies={showMovies} />
         </Fragment>
