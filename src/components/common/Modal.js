1db9a9b6f1d997f92ae486210800618f5448c1bb
tgafiulin <t.gafiulin@ya.ru>
2021-02-01 14:32:13 +0300

    add noscroll body when mpdal is open

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AuthForm from './AuthForm';
 import { useSelector, useDispatch } from 'react-redux'
 import { closeModal } from '../../app/modalReducer'
@@ -6,6 +6,16 @@ import { closeModal } from '../../app/modalReducer'
 function Modal() {
     const openModal = useSelector(state => state.modal.openModal)
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (openModal) {
+            document.body.style.overflow = 'hidden';
+        }
+
+        return function cleanup() {
+            document.body.style.overflow = 'auto';
+        };
+    })
 
     return (
         <div className={openModal ? 'modal-container modal-container--open' : 'modal-container'}>
