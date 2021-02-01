09af5481e6f23e7b212a64b168418204ca72e142
tgafiulin <t.gafiulin@ya.ru>
2021-02-01 15:05:29 +0300

    change scss structure

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Search from './Search'
 import AuthHeader from './AuthHeader'
+import { Link } from "react-router-dom"; 
 
 function Header() {
     return (
         <header className="header">
             <div className="container">
                 <div className="header__info">
-                    <div className="logo">
-                        <img src='/images/sign.svg' alt="Logo" className="logo__img" />
-                        <div className="logo__name">Видеосервис</div>
-                    </div>
+                    <Link to="/">
+                        <div className="logo">
+                            <img src='/images/sign.svg' alt="Logo" className="logo__img" />
+                            <div className="logo__name">Видеосервис</div>
+                        </div>
+                    </Link>
                     <Search />      
                     <AuthHeader />              
                 </div>
